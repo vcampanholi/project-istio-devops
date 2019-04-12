6775fa1302a254eecf034c98c37e61c1e67466ff
vcampanholi <Dimed*2019!>
2019-04-12 12:23:28 -0300

    Initial commit

---
+++ src/main/java/com/vcampanholi/istiodevops/IstioDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.vcampanholi.istiodevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstioDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstioDevopsApplication.class, args);
+	}
+
+}
