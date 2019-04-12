a4b53fd697afab9a3d4d0216638dd51e822fb884
vcampanholi <Dimed*2019!>
2019-04-12 13:32:04 -0300

    Settings build

--- src/test/java/com/vcampanholi/istiodevops/IstioDevopsApplicationTests.java
+++ src/test/java/com/vcampanholi/istiodevops/IstioDevopsApplicationTests.java
@@ -1,16 +1,15 @@
 package com.vcampanholi.istiodevops;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class IstioDevopsApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+
+    }
 
 }
